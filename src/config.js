ed71eb4674311dff9c24ef63fdd2a94c1843a67e
Huang Li-Pang <leo89214749@hotmail.com>
2018-07-10 09:23:36 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,16 +2,16 @@ module.exports = {
   /**
    * Output
    */
-  blogTitle: 'Unknow Me',
-  blogDescription: "Viko's Blog",
-  blogAuthor: 'viko16',
-  blogKeywords: 'Unknow Me, viko16, vue-ghpages-blog, 博客, vue',
+  blogTitle: "Huang Li-Pang's Blog",
+  blogDescription: "Huang Li-Pang's Blog",
+  blogAuthor: 'HuangLiPang',
+  blogKeywords: 'HuangLiPang',
   favicon: './static/favicon.png',
 
   /**
    * API
    */
-  repo: 'viko16/writings', // Which repository you stored the posts
+  repo: 'HuangLiPang/writings', // Which repository you stored the posts
   path: 'posts',
   branch: 'master'
 }
